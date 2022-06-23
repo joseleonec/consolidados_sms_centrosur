f54dab446b2a7122809a3a39cfe2d8ebf19f8e4d
joseleonec <joleonca@gmail.com>
2022-06-23 12:03:40 -0500

    Titulo del botón actualizado

--- app.py
+++ app.py
@@ -1,23 +1,3 @@
-# from flask import Flask, render_template, request
-# from werkzeug import secure_filename
-
-# app = Flask(__name__)
-
-# @app.route('/upload')
-# def upload_file():
-#    # return render_template('upload.html')
-#    return "hola"
-
-# @app.route('/uploader', methods = ['GET', 'POST'])
-# def uploader_file():
-#    if request.method == 'POST':
-#       f = request.files['file']
-#       f.save(secure_filename(f.filename))
-#       return 'file uploaded successfully'
-
-# if __name__ == '__main__':
-#    app.run(debug = True)
-
 from consolidación_de_campaña_sms import read_files
 
 import os
@@ -28,9 +8,7 @@ from werkzeug.utils import secure_filename
 main_html = 'upload.html'
 
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = "/uploads/"
 
-# Create a directory in a known location to save files to.
 uploads_dir = os.path.join(app.instance_path, '../uploads')
 os.makedirs(uploads_dir, exist_ok=True)
 
@@ -38,21 +16,10 @@ os.makedirs(uploads_dir, exist_ok=True)
 @app.route('/uploadfile', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        # save the single "profile" file
-        # remove files from uploads_ dir folder
-
         f = request.files.getlist('file')
         for i in f:
             i.save(os.path.join(uploads_dir, secure_filename(i.filename)))
         read_files(uploads_dir, f)
-        # profile = request.files['file']
-        # profile.save(os.path.join(uploads_dir, secure_filename(profile.filename)))
-
-        # save each "charts" file
-        # for file in request.files.getlist('charts'):
-        #     file.save(os.path.join(uploads_dir, secure_filename(file.name)))
-        # uploads = os.path.join(uploads_dir)
-
         try:
             filename = "consolidado.xlsx"
             print(uploads_dir)
@@ -69,45 +36,9 @@ def upload():
         return redirect(url_for('upload'))
     return render_template(main_html)
 
-
-# from flask import Flask, redirect, url_for, request, render_template
-# from werkzeug.utils import secure_filename
-# from consolidación_de_campaña_sms import read_files
-
-# app = Flask(__name__)
-
-
-# @app.route('/uploader', methods=['GET', 'POST'])
-# def uploader_file():
-#     if request.method == 'POST':
-#         # receive multiple files
-#         f = request.files.getlist('file')
-#         s = ""
-#         files = []
-#         for i in f:
-#             s += i.filename + "\n"
-#             files.append(i.filename)
-#             i.save(secure_filename(i.filename))
-#             # File(i.filename).save(secure_filename(i.filename))
-
-#         # return 'file uploaded successfully'
-#         return read_files(f)
-#         # return str(files)
-
-#         # f = request.files['file']
-#         # show the file name when uploaded
-#         # print(f.filename)
-#         # f.save(secure_filename(f.filename))
-#         # return 'file uploaded successfully'
-#         # return f.filename
-
-
-# @app.route('/success/<name>')
-# def success(name):
-#     return 'welcome %s' % name
-
 @app.route('/')
 def upload_file():
+    # remove files from uploads_ dir folder
     files = glob.glob(uploads_dir + '/*')
     for f in files:
         os.remove(f)
@@ -116,18 +47,6 @@ def upload_file():
         print("*"*50)
         
     return render_template(main_html)
-    # return "hola"
-
-
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     if request.method == 'POST':
-#         user = request.form['nm']
-#         return redirect(url_for('success', name=user))
-#     else:
-#         user = request.args.get('nm')
-#         return redirect(url_for('success', name=user))
-
 
 if __name__ == '__main__':
     # app.run(debug=False, host="0.0.0.0")
